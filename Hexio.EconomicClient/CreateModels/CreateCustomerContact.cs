1b9b13c2613abc7194f9ed3f34877fcb9aca444f
Morten Ricki Rasmussen <Morten@Hexio.dk>
2022-07-04 15:07:03 +0200

    Update CreateCustomerContact.cs

--- Hexio.EconomicClient/CreateModels/CreateCustomerContact.cs
+++ Hexio.EconomicClient/CreateModels/CreateCustomerContact.cs
@@ -3,5 +3,7 @@ namespace Hexio.EconomicClient.CreateModels
     public class CreateCustomerContact
     {
         public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
