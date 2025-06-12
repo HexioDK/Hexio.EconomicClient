faf8a19d77d74a3c09ca031a836a99a93f4af66d
Morten Ricki Rasmussen <Morten@Hexio.dk>
2025-06-12 16:01:14 +0200

    Update CustomerReadModel.cs

--- Hexio.EconomicClient/ReadModels/CustomerReadModel.cs
+++ Hexio.EconomicClient/ReadModels/CustomerReadModel.cs
@@ -31,6 +31,7 @@ namespace Hexio.EconomicClient.ReadModels
         public MetaData MetaData { get; set; }
         public Uri Self { get; set; }
         public bool Barred { get; set; }
+        public LayoutModel LayoutModel { get; set; }
         public IList<string> FieldsToFilter { get; } = new List<string>
         {
             "address","balance","barred","city","corporateIdentificationNumber","country","creditLimit","currency","customerNumber","ean","email","lastUpdated","mobilePhone","name","publicEntryNumber","telephoneAndFaxNumber","vatNumber","website","zip"
